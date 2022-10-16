9ab88ef8b90035b2555c4c5395998ae59e2b38b3
Jacek Treszczyński <devjac@ogarnij.se>
2022-10-16 13:56:11 +0200

    lua add operator

--- lua/fibaro.lua
+++ lua/fibaro.lua
@@ -1,31 +1,13 @@
-
-fibaro = {
-    getValue = function(a,b)
-        return 21
-    end,
-    debug = function (a,b)
-        print(a..b)
-    end
-}
-
-
 local hourOfTime = tonumber(os.date("%H"))
 local dayOfWeek = tonumber(os.date("%w"))
-local idDevice_1 = 114 --tu trzeba podac id pomiaru 1
-local idDevice_2 = 39 -- tu trzeba podać id pomiaru 2
-local controlDeviceId = 150 --tu trzeba podac id ktorym chcesz sterowac
+local device = {id1 = 114, id2 = 39, controlDeviceId = 150} -- tu trzeba podać id pomiaru 1 i 2
 local scena = "Scene195"
-
-local T1_1 = tonumber(fibaro.getValue(idDevice_1, "value"))
-local T2_1 = tonumber(fibaro.getValue(idDevice_2, "value"))
-local rangH = {        { start = 6, endd = 8 },        { start = 13, endd = 21 }    }
-rangH.add
 
 local temp = {    t1 = 20,    t2 = 18    }
 
 local grantM = { 
     name = "Pokoj Babci",
-    temp = tonumber(fibaro.getValue(idDevice_1, "value")),
+    temp = tonumber(fibaro.getValue(device.id1, "value")),
     items = 
     {
         {
@@ -39,7 +21,7 @@ local grantM = {
 
 local livingroom = {
     name = "Salon",
-    temp = tonumber(fibaro.getValue(idDevice_2, "value")),
+    temp = tonumber(fibaro.getValue(device.id2, "value")),
     items = 
         {
             {    
@@ -56,33 +38,34 @@ local livingroom = {
         }
 }
 
+function less(a, b) 
+    return a > b
+end
+
+function eqOrless(a, b) 
+    return a >= b
+end
 
 function openSwitch(room)
-
-    local itemsD = selectItemsByDay(room.items)
-    local itemsH = selectItemsByTime(itemsD)
+    local itemsD = selectItems(room.items, "rangD", dayOfWeek, eqOrless) 
+    local itemsH = selectItems(itemsD, "rangH", hourOfTime, less)
     local item = itemsH[0]
-    local tempInRomm = nil
-    if(table.getn(itemsH) > 0) then
-        tempInRomm = { isDay=true, temp=temp.t1 , name = "tryb dzin" }
-    else
-        tempInRomm = { isDay=false, temp=temp.t2, name = "tryb noc" }
-    end
+    local tempInRomm = getTempProces(itemsH)
 
     if tempInRomm.temp >= room.temp then
-        fibaro.debug(scena, "temperatura w " .. room.name .. " jest " .. room.temp .. " obecnie jest " .. item.name .. " jest " ..tempInRomm.name .. " zal" )
+        fibaro.debug(scena, "temperatura w " .. room.name .. " jest " .. room.temp .. " obecnie jest " .. item.name .. " jest " .. tempInRomm.name .. " zal" )
         return true
     end
-    fibaro.debug(scena, "temperatura w " .. room.name .. " jest " .. room.temp .. " obecnie jest " .. item.name .. " jest " ..tempInRomm.name .. " wyl" )
+    fibaro.debug(scena, "temperatura w " .. room.name .. " jest " .. room.temp .. " obecnie jest " .. item.name .. " jest " .. tempInRomm.name .. " wyl" )
     return false
 end
 
-function selectItemsByDay(items)
+function selectItems(items, name, value, operator )
     local result = {}
-    for k,item in pairs(items) do 
-        for k,rangDay in pairs(item.rangD) do 
+    for k,item in pairs(items) do         
+        for k,rangDay in pairs(item[name]) do 
             -- fibaro.debug(scena, "selectItemsByDay " .. dayOfWeek .. " TO " .. rangDay.start )
-            if rangDay.start <= dayOfWeek and rangDay.endd >= dayOfWeek then
+            if rangDay.start <= value and operator(rangDay.endd , value) then
                 table.insert( result, item)
             end
         end
@@ -90,27 +73,19 @@ function selectItemsByDay(items)
     return result
 end
 
-
-function selectItemsByTime(items)
-    local result = {}
-    for k,item in pairs(items) do 
-        for k, rang in pairs(item.rangH) do 
-            -- fibaro.debug(scena, "selectItemsByTime " .. hourOfTime .. " TO " .. rang.start .. " TO " .. rang.endd )
-            if rang.start <= hourOfTime and rang.endd > hourOfTime then
-                table.insert( result, item)
-            end
-        end
+function getTempProces(items) 
+    if(table.getn(itemsH) > 0) then
+        return  { isDay=true, temp=temp.t1 , name = "tryb dzin" }
+    else
+        return  { isDay=false, temp=temp.t2, name = "tryb noc" }
     end
-    return result
 end
 
-switchGrant = openSwitch(grantM)
-switchLiwingroom = openSwitch(livingroom)
 
-if switchGrant or switchLiwingroom then
+if openSwitch(grantM) or openSwitch(livingroom) then
     fibaro.debug(scena,"turnOn")
-    -- fibaro.call(controlDeviceId, "turnOff")
+    -- fibaro.call(device.controlDeviceId, "turnOff")
 else
     fibaro.debug(scena,"turnOff")
-    -- fibaro.call(controlDeviceId, "turnOn")
+    -- fibaro.call(device.controlDeviceId, "turnOn")
 end
